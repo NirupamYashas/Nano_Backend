9ce379a841b326abcf704a4c83edc3d19e11db11
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2024-03-28 02:34:43 -0400

    updated settings

--- Nano_Backend/settings.py
+++ Nano_Backend/settings.py
@@ -33,7 +33,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost',
-    'http://nano-tumor.phhp.ufl.edu'
+    'http://nanoqsar.phhp.ufl.edu'
 ]
 
 # Application definition
